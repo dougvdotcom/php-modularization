5a1ba1fdefc29a6457f7bc39c42c5a0a2612f616
Doug Vanderweide <dougvanderweide@gmail.com>
2016-02-22 09:16:47 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+<!--
+	PHP Modularization and Abstracting Demo
+	Copyright (C) 2008 Doug Vanderweide
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+			<!-- end #mainContent -->
+            </div>
+            <!-- This clearing element should immediately follow the #mainContent div in order to force the #container div to contain all child floats --><br class="clearfloat" />
+            <div id="footer">
+                <p>Footer</p>
+            <!-- end #footer -->
+            </div>
+        <!-- end #container -->
+        </div>
+    </body>
+</html>
